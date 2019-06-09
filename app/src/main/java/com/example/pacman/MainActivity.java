83d876dee639344bbd7470f7cf7ec48f6adaa2ed
Ihor Yankin <yankiniigor@gmail.com>
2019-06-09 23:59:38 +0300

    test

--- app/src/main/java/com/example/pacman/MainActivity.java
+++ app/src/main/java/com/example/pacman/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     AnimationDrawable pacmanAnimation;
 
     @Override
+    //Hi Ana
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
