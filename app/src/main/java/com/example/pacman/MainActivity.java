f68aa43d770acd88acf9af18cb12e104635ae4f0
Ihor Yankin <yankiniigor@gmail.com>
2019-06-10 00:10:06 +0300

    test

--- app/src/main/java/com/example/pacman/MainActivity.java
+++ app/src/main/java/com/example/pacman/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     //Hi Ana
-    //Hi
+    //GGG
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
