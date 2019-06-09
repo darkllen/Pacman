8d343b3088b1af863d4d574abeed411fb2ea786f
Ihor Yankin <yankiniigor@gmail.com>
2019-06-09 23:45:11 +0300

    added figure and its creation

---
+++ app/src/main/java/com/example/pacman/Map/FigureNum.java
@@ -0,0 +1,6 @@
+package com.example.pacman.Map;
+
+public enum FigureNum {
+    //each two numbers mean one line in array
+    FIGURE011100, FIGURE001011, FIGURE001101,FIGURE001110,FIGURE001100,FIGURE001010
+}
