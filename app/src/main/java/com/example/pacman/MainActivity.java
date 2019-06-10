af91ed44010c27b85630d72d2e3a0d49a29a721b
Ana22102000 <45357776+Ana22102000@users.noreply.github.com>
2019-06-10 18:53:55 +0300

    add simple map generation

--- app/src/main/java/com/example/pacman/MainActivity.java
+++ app/src/main/java/com/example/pacman/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.pacman;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+
+import com.example.pacman.Map.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +16,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     //Hi Ana
     //GGG
+    //<3
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ImageView imageView = (ImageView)findViewById(R.id.image);
-        imageView.setBackgroundResource(R.drawable.pacman_lose_animation);
+        imageView.setBackgroundResource(R.drawable.pacman_move_animation);
+
+        imageView.setRotation(90);
+        imageView.setRotation(180);
         pacmanAnimation = (AnimationDrawable)imageView.getBackground();
+        Map map=Map.generateMap();
     }
 
     //for pacman immediatly animation
---
+++ app/src/main/java/com/example/pacman/Map/Map.java
@@ -0,0 +1,188 @@
+package com.example.pacman.Map;
+
+import com.example.pacman.Map.Figure;
+
+public class Map {
+
+    private static int x = 5;
+    private static int y = 9;
+    private static int width = x * 3 + (x - 1) * 3 + 5;
+    private static int height = y * 3 + 3;
+    //карта на яку накладаються фігури. не поле для гри
+    private static int[][] startMap = new int[y + 2][x + 2];
+    //карта для гри
+    private static int[][] map = new int[height][width];
+
+
+    static int figureNumber = 1;
+
+    Map(int[][] map) {
+        this.map = map;
+    }
+
+    /**
+     * @return згенеровану карту
+     */
+    public static Map generateMap() {
+        fillStartMap();
+
+        figureNumber++;
+
+        boolean startAgain = false;
+
+        for (int j = 1; j < x + 1; j++)
+            for (int i = 1; i < y + 1; i++) {
+                Figure figure = FigureFabrique.getRandomFigure();
+                if (startMap[i][j] == 0) {
+                    startAgain = true;
+
+                    if (oneTileFigure(i, j)) {
+                        //add figure 1x1
+                        startMap[i][j] = figureNumber;
+                        figureNumber++;
+                        putFigureOnMap(i - 1, j, new Figure(new int[][]{{0, 0}, {1, 0}, {0, 0}}));
+                        i = 1;
+                        j = 1;
+                    } else
+
+                    if (addFigure(figure, i, j)) {
+                        //add figure
+                        for (int i1 = 0; i1 < 3; i1++)
+                            for (int j1 = 0; j1 < 2; j1++)
+                                if (figure.getType()[i1][j1] == 1)
+                                    startMap[i1 + i - 1][j1 + j] = figureNumber;
+                        figureNumber++;
+                        putFigureOnMap(i - 1, j, figure);
+                        i = 1;
+                        j = 1;
+                    }
+
+                }
+
+                if (i == y && j == x && startAgain) {
+                    startAgain = false;
+                    i = 1;
+                    j = 1;
+                }
+            }
+
+        //todo об'єднати деякі фігури
+
+        mirror();
+        return new Map(map);
+    }
+
+    /**
+     * mirrror map from right to left
+     */
+    private static void mirror() {
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width / 2; j++)
+                map[i][j] = map[i][width - j - 1];
+    }
+
+    /**
+     * заповнює початкові границі
+     */
+    private static void fillStartMap() {
+        //границі карти
+        for (int i = 0; i < y + 2; i++)
+            for (int j = 0; j < x + 2; j++)
+                startMap[i][j] = 1;
+        for (int i = 1; i < y + 1; i++)
+            for (int j = 1; j < x + 1; j++)
+                startMap[i][j] = 0;
+        //home for monsters
+        startMap[4][2] = 1;
+        startMap[5][2] = 1;
+        startMap[4][1] = 1;
+        startMap[5][1] = 1;
+
+        //границі карти
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
+                map[i][j] = 1;
+        for (int i = 1; i < height - 1; i++)
+            for (int j = 2; j < width - 2; j++)
+                map[i][j] = 0;
+        //home for monsters
+        for (int i = 11; i < 16; i++)
+            for (int j = 12; j < 20; j++)
+                map[i][j] = 1;
+        for (int i = 12; i < 15; i++)
+            for (int j = 13; j < 19; j++)
+                map[i][j] = 0;
+        map[11][15] = 0;
+        map[11][16] = 0;
+
+    }
+
+    /**
+     * малює фігуру на мапі
+     *
+     * @param i      координати верхнього лівого кутка
+     * @param j
+     * @param figure фігура
+     */
+    private static void putFigureOnMap(int i, int j, Figure figure) {
+        int x1;
+        int y1;
+
+        for (int k = 0; k < 3; k++)
+            for (int l = 0; l < 2; l++) {
+                //координата лівої верхньої точки клітинки 3х3
+                y1 = (x - 1) * 3 + 3 + (j - 1) * 3 + l * 3;
+                x1 = 2 + (i - 1) * 3 + k * 3;
+
+                if (y1 > 0 && x1 > 0)
+                    //малюємо клітинку фігури
+                    if (figure.getType()[k][l] == 1) {
+                        map[x1][y1] = startMap[i + 1][j];
+                        map[x1 + 1][y1] = startMap[i + 1][j];
+                        map[x1][y1 + 1] = startMap[i + 1][j];
+                        map[x1 + 1][y1 + 1] = startMap[i + 1][j];
+
+                        if (l != 1) if (figure.getType()[k][l + 1] == 1) {
+                            map[x1][y1 + 2] = startMap[i + 1][j];
+                            map[x1 + 1][y1 + 2] = startMap[i + 1][j];
+                        }
+                        if (k != 2) if (figure.getType()[k + 1][l] == 1) {
+                            map[x1 + 2][y1] = startMap[i + 1][j];
+                            map[x1 + 2][y1 + 1] = startMap[i + 1][j];
+                        }
+                    }
+            }
+    }
+
+
+    /**
+     * @param figure фігура яка накладається
+     * @param x1     куди накладеться 2 рядок фігури
+     * @param y1     куди накладеться 1 стовпчик фігури
+     * @return чи можна накласти фігуру
+     */
+    private static boolean addFigure(Figure figure, int x1, int y1) {
+        for (int i = x1 - 1; i < x1 + 2; i++)
+            for (int j = y1; j < y1 + 2; j++)
+                if (startMap[i][j] > 0 && figure.getType()[i - (x1 - 1)][j - y1] > 0) return false;
+        return true;
+    }
+
+    /**
+     * @param i координати клітинки
+     * @param j
+     * @return чи є ця клітинка одинарною фігурою
+     */
+    private static boolean oneTileFigure(int i, int j) {
+        if (startMap[i - 1][j] == 0) return false;
+        if (startMap[i + 1][j] == 0) return false;
+        if (startMap[i][j - 1] == 0) return false;
+        if (startMap[i][j + 1] == 0) return false;
+        return true;
+    }
+
+    public int[][] getMap() {
+        return map;
+    }
+
+}
