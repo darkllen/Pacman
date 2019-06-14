767a8332b4898258bb8d02db2d25363ca5313e6c
Ana22102000 <45357776+Ana22102000@users.noreply.github.com>
2019-06-14 21:33:42 +0300

    .

---
+++ app/src/main/java/Menu/GameUsualActivity.java
@@ -0,0 +1,248 @@
+package Menu;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.media.AudioManager;
+import android.media.AudioRouting;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.pacman.Map.Map;
+import com.example.pacman.R;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import Units.Pacman;
+//class for classic game
+public class GameUsualActivity extends AppCompatActivity {
+    //additional variables for define swap
+    final int[] clickX = {0};
+    final int[] clickY = {0};
+    final int[] clickStartX = {0};
+    final int[] clickStartY = {0};
+    final boolean[] first = {true};
+    //1 - move right, 2- move left, 3 - move down, 4 - move up
+    final int[] click = {-1};
+
+    MediaPlayer pacman_police;
+    MediaPlayer pacman_begin;
+
+    private Handler handler;
+
+
+
+    @SuppressLint({"ClickableViewAccessibility", "HandlerLeak"})
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //hide top panel
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.game_usual);
+        ConstraintLayout layout = findViewById(R.id.pacmanLayout);
+
+        pacman_begin = MediaPlayer.create(this,R.raw.pacman_beginning);
+        pacman_begin.start();
+        pacman_police = MediaPlayer.create(this, R.raw.pacman_police);
+
+
+        pacman_begin.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                pacman_police.start();
+                pacman_police.setLooping(true);//todo there is a little pause before starting to play again - cut music file (pacman_chomp)
+                pacman_police.setVolume(100,100);//??
+            }
+        });
+
+
+
+        //int side = 1000/30;
+//        Display display = getWindowManager().getDefaultDisplay();//todo передавати параметр ширини в пакмана??
+//        Point size = new Point();
+//        display.getSize(size);
+//        int width = size.x;
+//        int height = size.y;
+        int width=1080;
+        int side=width/26;
+        System.out.println(width);
+
+        layout.setBackgroundColor(Color.BLACK);
+        //generate map and create black images for walls
+        Map map = Map.generateMap();
+        int[][] m = map.getMap();
+
+        ImageView[][] views = new ImageView[m.length][m[1].length];
+
+
+        for (int i = 0; i<m.length;i++){
+            for (int j = 0; j<m[i].length;j++){
+                ImageView imageView = new ImageView(this);
+
+                ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(side,side);
+                imageView.setLayoutParams(params);
+
+
+
+                // add images
+                int left,right,up,down;//numbers on the sides
+                boolean p=true;
+
+                //границі
+                if(i==1||i==m.length-2)  {imageView.setImageResource(R.drawable.tile2);imageView.setRotation(0);p=false;}
+                if(j==0||j==m[0].length-1)   {imageView.setImageResource(R.drawable.tile2);imageView.setRotation(90);p=false;}
+                //кути
+                if(i==1&&j==m[0].length-1){imageView.setImageResource(R.drawable.tile1);imageView.setRotation(90);p=false;}
+                if(i==m.length-2&&j==0) {imageView.setImageResource(R.drawable.tile1);imageView.setRotation(270);p=false;}
+                if(i==1&&j==0)  {imageView.setImageResource(R.drawable.tile1);imageView.setRotation(180);p=false;}
+                if(i==m.length-2&&j==m[0].length-1) {imageView.setImageResource(R.drawable.tile1);imageView.setRotation(0);p=false;}
+
+                //поворот в тунель
+                if (p==false){
+                    if(i==1&&m[0][j]==1&&m[1][j+1]==-1)  { imageView.setImageResource(R.drawable.tile1);imageView.setRotation(0);}
+                    if(i==1&&m[0][j]==1&&m[1][j-1]==-1)  { imageView.setImageResource(R.drawable.tile1);imageView.setRotation(-90);}
+                    if(i==m.length-2&&m[m.length-1][j]==1&&m[m.length-2][j-1]==-1)  { imageView.setImageResource(R.drawable.tile1);imageView.setRotation(180);}
+                    if(i==m.length-2&&m[m.length-1][j]==1&&m[m.length-2][j+1]==-1)  { imageView.setImageResource(R.drawable.tile1);imageView.setRotation(90);}
+
+
+                }
+
+                //порожні клітинки
+                if (m[i][j]==0||m[i][j]==-1) {imageView.setImageResource(R.drawable.tile3);}
+                //порожні клітинки та тунель
+                if(i==0||i==m.length-1){p=false;if(m[i][j]==1){imageView.setImageResource(R.drawable.tile2);imageView.setRotation(90);}}
+                //непорожні клітинки
+                if(m[i][j]==1&&p){
+                        left=m[i-1][j];
+                        right=m[i+1][j];
+                        up=m[i][j-1];
+                        down=m[i][j+1];
+
+                        //полостки
+                    if(left==1&&right==1)
+                    {imageView.setImageResource(R.drawable.tile2);imageView.setRotation(90);}
+
+                    if(up==1&&down==1)
+                    {imageView.setImageResource(R.drawable.tile2);imageView.setRotation(0);}
+
+                    //кути
+                    if((right==0&&down==0)||(left==1&&right==1&&up==1&&down==1&&m[i-1][j-1]==0))
+                    { imageView.setImageResource(R.drawable.tile1);imageView.setRotation(0);}
+
+                    if((left==0&&down==0)||(left==1&&right==1&&up==1&&down==1&&m[i+1][j-1]==0))
+                    {imageView.setImageResource(R.drawable.tile1);imageView.setRotation(90);}
+
+                    if((left==0&&up==0)||(left==1&&right==1&&up==1&&down==1&&m[i+1][j+1]==0))
+                    {imageView.setImageResource(R.drawable.tile1);imageView.setRotation(180);}
+
+                    if((right==0&&up==0)||(left==1&&right==1&&up==1&&down==1&&m[i-1][j+1]==0))
+                    {imageView.setImageResource(R.drawable.tile1);imageView.setRotation(-90);}
+}
+
+                 imageView.setX(i*side);
+                imageView.setY(j*side+100);
+                layout.addView(imageView);
+            }
+        }
+
+        //add bonus
+        Bonus.Point[][]bonus=map.getBonus();
+        for (int i = 0; i<m.length;i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                ImageView imageView = new ImageView(this);
+                views[i][j] = imageView;
+                ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(side, side);
+                imageView.setLayoutParams(params);
+
+                if(bonus[i][j].getType()==1)imageView.setImageResource(R.drawable.point);else
+                if(bonus[i][j].getType()==2)imageView.setImageResource(R.drawable.big_point);
+
+                imageView.setX(i * side);
+                imageView.setY(j * side + 100);
+                layout.addView(imageView);
+            }
+        }
+
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                layout.removeView(views[msg.arg1][msg.arg2]);
+            }
+        };
+        //create new Thread for pacman unit
+        Pacman pacman = new Pacman(findViewById(R.id.image), m,this,layout, handler);
+        pacman.start();
+
+        layout.setOnTouchListener(new RecordFirstAndLastCoordinatesOnTouchListener());
+        layout.setOnClickListener(new ChangeMoveOnClickListener(pacman));
+
+
+    }
+
+/*    @Override
+    public void onPause() {
+        super.onPause();
+        pacman_police.pause();
+        // stop the clock
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        pacman_police.start();
+    }*/
+
+    //listener for record x and y to decide side of swap
+    private class RecordFirstAndLastCoordinatesOnTouchListener implements View.OnTouchListener{
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (first[0]){
+                clickStartX[0] = (int) event.getX();
+                clickStartY[0] = (int) event.getY();
+                first[0] = false;
+            }
+            clickX[0] = (int) event.getX();
+            clickY[0] = (int) event.getY();
+            return false;
+        }
+    }
+    //listener for the end of the swap, that change pacman movement
+    private class ChangeMoveOnClickListener implements View.OnClickListener{
+        Pacman pacman;
+
+        ChangeMoveOnClickListener(Pacman pacman) {
+            this.pacman = pacman;
+        }
+        @Override
+        public void onClick(View v) {
+            first[0] = true;
+            int differenceX = clickX[0] - clickStartX[0];
+            int differenceY = clickY[0] - clickStartY[0];
+            if (Math.abs(differenceX)>=Math.abs(differenceY)){
+                if (differenceX>0){
+                    click[0] = 1;
+                }else {
+                    click[0] = 2;
+                }
+            }else {
+                if (differenceY>0){
+                    click[0] =3;
+                }else {
+                    click[0] =4;
+                }
+            }
+            pacman.changeMove(click[0]);
+        }
+    }
+}
---
+++ app/src/main/java/Units/Pacman.java
@@ -0,0 +1,257 @@
+package Units;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
+import android.os.Message;
+import android.support.constraint.ConstraintLayout;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+
+import com.example.pacman.Map.Map;
+import com.example.pacman.R;
+
+import Menu.GameUsualActivity;
+
+public class Pacman extends Thread {
+    //constants, shouldnt be changed in running time
+    private ImageView imageView;
+    //unnecessary value
+    private final ObjectAnimator[] animator = {ObjectAnimator.ofFloat(imageView, "X",1000)};
+    private final AnimatorSet[] set = {new AnimatorSet()};
+    private int startX;
+    private int startY;
+
+    //variables to control area of movement and speed
+    private int leftDestination = 0;
+    private int rightDestination = 1000;
+    private int upDestination = 0;
+    private int bottomDestination = 2000;
+    private int speedInPixelsForSecond = 300;
+
+    int[][] map;
+
+    //position in map
+    int xMap = 12;
+    int yMap = 28;
+
+    //values in numbers are tested and not accurate(
+
+    GameUsualActivity gUA;
+    ConstraintLayout layout;
+    Handler handler;
+
+    public Pacman(ImageView imageView, int[][] map,GameUsualActivity gUA,ConstraintLayout layout,  Handler handler) {
+        this.imageView = imageView;
+        this.map = map;
+        this.gUA=gUA;
+        this.layout=layout;
+        this.handler = handler;
+        startX = 1080/26*xMap;
+        startY = 1080/26*yMap;
+        map[xMap][yMap] = 2;
+        imageView.setX(startX);
+        imageView.setY(startY+ 100);
+    }
+
+    @Override
+    public void run() {
+        //set pacman animation and start location
+        imageView.setBackgroundResource(R.drawable.pacman_move_animation);
+        AnimationDrawable pacmanAnimation = (AnimationDrawable) imageView.getBackground();
+        imageView.setY(startY+100);
+        imageView.setX(startX);
+        imageView.bringToFront();
+        pacmanAnimation.start();
+
+
+
+        while (true){
+            int currX = (Math.round((imageView.getX())/(1080/26)));
+            int currY = Math.round (((imageView.getY()-100)/(1080/26)));
+            map[xMap][yMap] = 0;
+            xMap = currX;
+            yMap = currY;
+            if (Map.getBonus()[xMap][yMap].getType() != 0) {
+        Map.setScore(Map.getScore() + Map.getBonus()[xMap][yMap].getScore());
+        Map.setOneBonus(xMap, yMap, 0);
+        //todo delete bonus (draw tiles3.png)
+                Message msg = new Message();
+                msg.obj = xMap + " " + yMap;
+                msg.arg1 = xMap;
+                msg.arg2 = yMap;
+                handler.sendMessage(msg);
+
+    }
+        }
+
+
+//    //detele point and plus score + check if there is invisible bonus to change monster`s mode
+//    if (Map.getBonus()[xMap][yMap].getType() != 0) {
+//        Map.setScore(Map.getScore() + Map.getBonus()[xMap][yMap].getScore());
+//        Map.setOneBonus(xMap, yMap, 0);
+//        //todo delete bonus (draw tiles3.png)
+//        ImageView imageView = new ImageView(gUA);
+//        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(1080 / 31, 1080 / 31);
+//        imageView.setLayoutParams(params);
+//        imageView.setImageResource(R.drawable.tiles3);
+//
+//        imageView.setX(xMap * 1080 / 31);
+//        imageView.setY(yMap * 1080 / 31 + 100);
+//        layout.addView(imageView);
+//    }
+
+    }
+
+    /**
+     *
+     * @param change change pacman movement acording to the variable
+     *               1 - right
+     *               2 - left
+     *               3 - bottom
+     *               4 - up
+     *               area and speed depends on global variables and calculated each time.
+     */
+    public void changeMove(int change){
+        //set current location on swap
+
+        startX = 1080/26*xMap;
+        startY = 1080/26*yMap;
+        imageView.setX(startX);
+        imageView.setY(startY+ 100);
+        float xNew;
+        long time;
+        int t = 0;
+
+        switch (change){
+            case 1:
+                t = xMap;
+                for (int i = xMap; i< map.length; i++){
+                    if (map[i][yMap]!=1){
+                        t = i;
+                    }else {
+                        if (t ==xMap)return;
+                        map[xMap][yMap] = 0;
+                        xMap = t;
+                        map[xMap][yMap] = 2;
+                        break;
+                    }
+                }
+                rightDestination = 1080/26*(t);
+                imageView.setRotation(0);
+                xNew = imageView.getX();
+                set[0].cancel();
+                set[0] = new AnimatorSet();
+                set[0].setInterpolator(new LinearInterpolator());
+                imageView.setX(xNew);
+                animator[0] = ObjectAnimator.ofFloat(imageView, "X", rightDestination);
+                set[0].play(animator[0]);
+                time = (long) ((rightDestination - xNew)/speedInPixelsForSecond*1000);
+                set[0].setDuration(time);
+                set[0].start();
+                break;
+            case 2:
+                t = xMap;
+                for (int i = xMap; i>0;i--){
+                    if (map[i][yMap]!=1){
+                        t = i;
+                    }else {
+                        if (t ==xMap)return;
+                        map[xMap][yMap] = 0;
+                        xMap = t;
+                        map[xMap][yMap] = 2;
+                        break;
+                    }
+                }
+                leftDestination = 1080/26*(t);
+                imageView.setRotation(180);
+                xNew = imageView.getX();
+                set[0].cancel();
+                set[0] = new AnimatorSet();
+                set[0].setInterpolator(new LinearInterpolator());
+                imageView.setX(xNew);
+                animator[0] = ObjectAnimator.ofFloat(imageView, "X", leftDestination);
+                set[0].play(animator[0]);
+                time = (long) ((xNew - leftDestination)/speedInPixelsForSecond*1000);
+                set[0].setDuration(time);
+                set[0].start();
+                break;
+            case 3:
+                t = xMap;
+                for (int i = yMap; i< map[xMap].length; i++){
+                    if (map[xMap][i]!=1){
+                        t = i;
+                    }else {
+                        if (t ==yMap)return;
+                        map[xMap][yMap] = 0;
+                        yMap = t;
+                        map[xMap][yMap] = 2;
+                        break;
+                    }
+                }
+                bottomDestination = 1080/26*(t)+100;
+                imageView.setRotation(90);
+                xNew = imageView.getY();
+                set[0].cancel();
+                set[0] = new AnimatorSet();
+                set[0].setInterpolator(new LinearInterpolator());
+                imageView.setY(xNew);
+                animator[0] = ObjectAnimator.ofFloat(imageView, "Y", bottomDestination);
+                set[0].play(animator[0]);
+                time = (long) ((bottomDestination-xNew)/speedInPixelsForSecond*1000);
+                set[0].setDuration(time);
+                set[0].start();
+                break;
+            case 4:
+                t = xMap;
+                for (int i = yMap; i>0;i--){
+                    if (map[xMap][i]!=1){
+                        t = i;
+                    }else {
+                        if (t ==yMap)return;
+                        map[xMap][yMap] = 0;
+                        yMap = t;
+                        map[xMap][yMap] = 2;
+                        break;
+                    }
+                }
+                upDestination = 1080/26*(t)+100;
+                imageView.setRotation(270);
+                xNew = imageView.getY();
+                set[0].cancel();
+                set[0] = new AnimatorSet();
+                set[0].setInterpolator(new LinearInterpolator());
+                imageView.setY(xNew);
+                animator[0] = ObjectAnimator.ofFloat(imageView, "Y", upDestination);
+                set[0].play(animator[0]);
+                time = (long) ((xNew- upDestination)/speedInPixelsForSecond*1000);
+                set[0].setDuration(time);
+                set[0].start();
+        }
+
+
+
+    }
+
+    public void setLeftDestination(int leftDestination) {
+        this.leftDestination = leftDestination;
+    }
+
+    public void setRightDestination(int rightDestination) {
+        this.rightDestination = rightDestination;
+    }
+
+    public void setUpDestination(int upDestination) {
+        this.upDestination = upDestination;
+    }
+
+    public void setBottomDestination(int bottomDestination) {
+        this.bottomDestination = bottomDestination;
+    }
+
+    public void setSpeedInPixelsForSecond(int speedInPixelsForSecond) {
+        this.speedInPixelsForSecond = speedInPixelsForSecond;
+    }
+}
